a3340e0bb0dfa88e6837a1219c0091dfb8d945ed
Jonathan Evans <jonathan.ca.evans@gmail.com>
2022-03-24 22:46:46 -0400

    Added question2

--- question2.py
+++ question2.py
@@ -2,22 +2,60 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 
-def main():
-    n = 250
-    #create list with all core speeds
-    speeds = [2,2,2,4,4,4]
-    #create list with all core names
-    names = ["PA", "PB", "PC", "PD", "PE", "PF"]
-    #create dataframe with core speeds and names
-    df = pd.DataFrame({"Speed": speeds, "Name": names})
-    #create list of runtimes
-    df["Burst Times"] = np.random.randint(10000000,100000000000, size = 6)
-    #create list of memory requirements
-    df["Memory"] = np.random.randint(1,16000, size = 6)
-    print(df)
-    #sort dataframe by increasing speed
-    df.sort_values(by = "Speed", inplace = True)
-    print(df)
-    #initalize total runtime and total memory
-    total_runtime = 0
-    total_memory = 0
+def heterogenous(df):
+    clockSpeeds = [1,1,1,2,2,2]
+    turnaround = 0
+    wait = 0
+
+    processes = df.to_numpy().tolist()
+
+    processors = []
+    for i in range(6):
+        processors.append(0)
+
+    queues = []
+    for i in range(6):
+        queues.append([])
+
+    processes.sort(key = lambda x: x[1])
+    
+    while len(processes) > 5:
+       a,b,c = processes.pop(0),processes.pop(0),processes.pop(0)
+
+       i = 0
+
+       while((i < len(processes) - 3) and (a[1] < processes[i][1]/2)):
+           i+=1
+
+       d,e,f = processes.pop(i),processes.pop(i),processes.pop(i)
+
+       queues[0].append(a)
+       queues[1].append(b)
+       queues[2].append(c)
+       queues[3].append(d)
+       queues[4].append(e)
+       queues[5].append(f)
+
+    i=5
+    while len(processes) > 0:
+       queues[i].append(processes.pop())
+       i-=1
+
+    #Calculate wait and turnaround
+    for i in range(6):
+       for process in queues[i]:
+           if i < 3:
+               wait += processors[i]
+               processors[i] += process[1]
+               turnaround += process[i]
+           else:
+               wait += processors[i]
+               processors[i] += process[1]  
+               turnaround += processors[i]
+
+    return turnaround/df.shape[0], wait/df.shape[0]
+
+if __name__ == "__main__":
+    df = pd.read_csv('processes.csv')
+    print(heterogenous(df))
+    
