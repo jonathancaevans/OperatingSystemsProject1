eca21824c378f608f469f8235969010240212807
omarkmu <omarkmu@gmail.com>
2022-03-24 22:52:20 -0400

    Add question1 RR

--- question1.py
+++ question1.py
@@ -1,3 +1,4 @@
+from collections import deque
 import pandas as pd
 import numpy as np
 
@@ -55,7 +56,40 @@ def SJF(df, n):
 
     return turnaround/df.shape[0], wait/df.shape[0]
 
+def RR(df, n, quantum):
+    # list of burst & remaining times
+    ready = deque([rec[1], rec[1]] for rec in df.to_numpy().tolist())
+
+    turnaround = 0
+    wait = 0
+
+    # contains currently executing processes
+    processors = deque(ready.popleft() for _ in range(n))
+    elapsed = [0] * n # total elapsed time per processor
+
+    while len(processors) > 0:
+        # visit each current process
+        for i in range(len(processors)):
+            process = processors.popleft()
+            p_burst, p_rem = process
+
+            if p_rem > quantum:
+                process[1] -= quantum
+                elapsed[i] += quantum
+                ready.append(process)
+            else:
+                elapsed[i] += p_rem
+                turnaround += elapsed[i]
+                wait += elapsed[i] - p_burst
+
+            if len(ready) > 0:
+                processors.append(ready.popleft())
+    
+    return turnaround/df.shape[0], wait/df.shape[0]
+
+
 if __name__ == "__main__":
     df = pd.read_csv('processes.csv')
     print(FIFO(df,6))
     print(SJF(df,6))
+    print(RR(df, 6, 10**6 + (10**12 - 10**6)//2))
